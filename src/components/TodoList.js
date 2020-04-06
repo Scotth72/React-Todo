6b517ec8ae81fc82e5762ea6d036090612f03902
Heath Scott <HeathScott@Heaths-MacBook-Pro.local>
2020-04-06 17:47:17 -0500

    Working on Project

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import ToDoList from './components/ToDoList';
+
+
+const ToDoList = () => {
+
+    return(
+
+
+    )
+};
+
+export default ToDoList;
